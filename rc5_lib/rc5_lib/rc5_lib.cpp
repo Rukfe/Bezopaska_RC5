201618db123bdc54dddb70309ef7d55c424abb5f
Denis <78860817+ChernavinD@users.noreply.github.com>
2023-11-28 15:25:15 +0300

    pybind11, rc5_lib
    Добавил папку 'extern' для внешних зависимостей. Туда же добавил 'pybind11'.
    
    Создал проект 'rc5_lib'. Описал структуру ключа с базовыми действиями (создание объекта, удаление объекта с очисткой памяти, инициализация ключа (размер, значение)), константы, макросы и т.д.

---
+++ rc5_lib/rc5_lib/rc5_lib.cpp
@@ -0,0 +1,58 @@
+﻿// rc5_lib.cpp: определяет точку входа для приложения.
+//
+
+#include "rc5_lib.h"
+
+using namespace std;
+
+rc5UserKey* RC5_Key_Create()
+{
+	rc5UserKey* pKey;
+
+	pKey = (rc5UserKey*)malloc(sizeof(*pKey));
+	if (pKey != ((rc5UserKey*)0))
+	{
+		pKey->keyLength = 0;
+		pKey->keyBytes = (unsigned char*)0;
+	}
+	return (pKey);
+}
+
+void RC5_Key_Destroy(rc5UserKey* pKey)
+{
+	unsigned char*	to;
+	int             count;
+
+	if (pKey == ((rc5UserKey*)0))
+		return;
+	if (pKey->keyBytes == ((unsigned char*)0))
+		return;
+	to = pKey->keyBytes;
+	for (count = 0; count < pKey->keyLength; count++)
+		*to++ = (unsigned char)0;
+	free(pKey->keyBytes);
+	pKey->keyBytes = (unsigned char*)0;
+	pKey->keyLength = 0;
+	free(pKey);
+}
+
+int RC5_Key_Set(rc5UserKey* pKey,
+	int						keyLength,
+	unsigned char*			keyBytes
+)
+{
+	unsigned char*	keyBytesCopy;
+	unsigned char*	from, * to;
+	int				count;
+
+	keyBytesCopy = (unsigned char*)malloc(keyLength);
+	if (keyBytesCopy == ((unsigned char*)0))
+		return (0);
+	from = keyBytes;
+	to = keyBytesCopy;
+	for (count = 0; count < keyLength; count++)
+		*to++ = *from++;
+	pKey->keyLength = count;
+	pKey->keyBytes = keyBytesCopy;
+	return (1);
+}
---
+++ rc5_lib/rc5_lib/rc5_lib.h
@@ -0,0 +1,82 @@
+﻿// rc5_lib.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include "../../extern/pybind11/include/pybind11/pybind11.h"
+
+namespace py = pybind11;
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+
+/* Definition of RC5 user key object. */
+typedef struct rc5UserKey
+{
+	int				keyLength; /* In Bytes. */
+	unsigned char*	keyBytes;
+}	rc5UserKey;
+
+/*
+	Allocate and initialize an RC5 user key.
+	Return 0 if problems.
+ */
+rc5UserKey* RC5_Key_Create();
+
+/*
+	Zero and free an RC5 user key.
+ */
+void RC5_Key_Destroy(rc5UserKey* pKey);
+
+/*
+	Set the value of an RC5 user key.
+	Copy the key bytes so the caller can zero and
+	free the original.
+	Return zero if problems
+ */
+int RC5_Key_Set(rc5UserKey* pKey,
+	int						keyLength,
+	unsigned char*			keyBytes
+);
+
+/*	
+	Definitions for RC5 as a 64 bit block cipher.
+	The "unsigned int" will be 32 bits on all but
+	the oldest compilers, which will make it 16 bits.
+	On a DEC Alpha "unsigned long" is 64 bits, not 32. 
+ */
+#define RC5_WORD     unsigned int
+#define W            (32)
+#define WW           (W / 8)
+#define ROT_MASK     (W - 1)
+#define BB           ((2 * W) / 8) /* Bytes per block */
+
+/*	
+	Define macros used in multiple procedures.
+	These macros assumes ">>" is an unsigned operation,
+	and that x and s are of type RC5_WORD. 
+ */
+#define SHL(x,s)    ((RC5_WORD)((x)<<((s)&ROT_MASK)))
+#define SHR(x,s,w)  ((RC5_WORD)((x)>>((w)-((s)&ROT_MASK))))
+#define ROTL(x,s,w) ((RC5_WORD)(SHL((x),(s))|SHR((x),(s),(w))))
+
+#if W == 16
+#define P16  0xb7e1
+#define Q16  0x9e37
+#define Pw   P16 /* Select 16 bit word size */
+#define Qw   Q16
+#endif
+
+#if W == 32
+#define P32  0xb7e15163
+#define Q32  0x9e3779b9
+#define Pw   P32 /* Select 32 bit word size */
+#define Qw   Q32
+#endif
+
+#if W == 64
+#define P64  0xb7e151628aed2a6b
+#define Q64  0x9e3779b97f4a7c15
+#define Pw   P64 /* Select 64 bit word size */
+#define Qw   Q64
+#endif

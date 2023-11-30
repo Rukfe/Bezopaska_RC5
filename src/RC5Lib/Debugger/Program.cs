008ed0aed849f29c446bead767a29f0cc65e83d1
Denis <78860817+ChernavinD@users.noreply.github.com>
2023-11-30 15:46:11 +0300

    random

--- src/RC5Lib/Debugger/Program.cs
+++ src/RC5Lib/Debugger/Program.cs
@@ -3,18 +3,17 @@ using System.Text;
 using RC5Lib;
 
 string key = RC5.KeyGen();
-Console.WriteLine("Ключ: " + key);
+Console.WriteLine("Ключ: " + key + '\n');
 
-string encrypted = RC5.Encrypt("I am a storm that is approaching...", key);
+
+string message = "Блять";
+string encrypted, decrypted;
+
+encrypted = RC5.Encrypt(message, key);
 Console.WriteLine("Зашифрованное сообщение: " + encrypted);
 
-string decrypted = RC5.Decrypt(encrypted, key);
+decrypted = RC5.Decrypt(encrypted, key);
 Console.WriteLine("Расшифрованное сообщение: " + decrypted + "\n");
-
-
-string message = "Я ебал в рот (В рот) тех, кто не ебал в рот тех, кто ебал в жопу";
-key = "Я на седьмом этаже, это как шестой, но на один повыше";
-Console.WriteLine("Оригинальное сообщение: " + message);
 
 encrypted = RC5.Encrypt(message, key);
 Console.WriteLine("Зашифрованное сообщение: " + encrypted);
